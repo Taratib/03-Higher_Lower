6be2e936710bb407cb948ad9e6503cfaad3e18ce
Tarati <99302555+Taratib@users.noreply.github.com>
2022-05-19 12:59:35 +1200

    Add files via upload

--- 08_HL_score.py
+++ 08_HL_score.py
@@ -1,12 +1,12 @@
 # RPS Component 6 - Soring System
-
+game_summary = []
 # Rounds won will be calculated (total - draw - lost)
 rounds_played = 0
 rounds_lost = 0
 rounds_drawn = 0
 
 # Results for testing purposes
-test_results = ["best", "best", "worst", "worst", "average"]
+test_results = ["won", "won", "loss", "loss", "tie"]
 
 # PLay Game
 for item in test_results:
@@ -17,7 +17,7 @@ for item in test_results:
     result = item
 
     if result == "tie":
-        result == "It'a a tie"
+        result == "It's a tie"
         rounds_drawn += 1
     elif result == "loss":
         rounds_lost += 1
@@ -29,6 +29,18 @@ rounds_won = rounds_played - rounds_lost - rounds_drawn
 # End of Game Statements
 print()
 print('***** End Game Summary *****')
-print("Best: {} \t|\t Worst: {} \t|\t Average: {}".format(rounds_won, rounds_lost, rounds_drawn))
+print("Won: {} \t|\t Lost: {} ".format(rounds_won, rounds_lost, rounds_drawn))
 print()
-print("Thanks for playing")
+
+game_summary = input("Please press <Enter> to see your game summary / statistics...").lower()
+
+print()
+print("**** Game Scores *******")
+for game in game_summary:
+    print(game)
+
+print()
+
+# display game stats with $ values to the nearest whole number
+print("******* Game Statistics ********")
+print("Best: {} \nWorst: {} \nAverage: {}".format(rounds_won, rounds_lost, rounds_drawn))
